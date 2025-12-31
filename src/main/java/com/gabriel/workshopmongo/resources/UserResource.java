fb448e37593521ac084292ae9608475c10de6305
Gabriel Wodtke <gabrielwodtke28@gmail.com>
2025-12-30 21:41:06 -0300

    Obtendo um usuário por id

--- src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/workshopmongo/resources/UserResource.java
@@ -5,10 +5,7 @@ import com.gabriel.workshopmongo.dto.UserDTO;
 import com.gabriel.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,4 +24,13 @@ public class UserResource {
         List<UserDTO> listDTO = list.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(listDTO);
     }
+
+    @GetMapping(value = "/{id}")
+    public UserDTO findById(@PathVariable String id){
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(obj)).getBody();
+    }
+
+
 }
